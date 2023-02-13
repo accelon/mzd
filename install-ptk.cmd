14cc4f1777924df704fb83ac29ea5209fc111b9c
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2023-02-13 16:07:23 +0800

    typo\

--- install-ptk.cmd
+++ install-ptk.cmd
@@ -4,4 +4,4 @@ cmd/c ptk js mzd-en
 if not exist "..\accelon22\dist\mzd" ( mklink/j ..\accelon22\dist\mzd mzd )
 if not exist "..\accelon22\dist\mzd-en" ( mklink/j ..\accelon22\dist\mzd-en mzd-en )
 
-echo window.accelon2={preload:"mzd,mzd-en"} > ..\accelon22\dist\config.js
+echo window.accelon22={preload:"mzd,mzd-en"} > ..\accelon22\dist\config.js
