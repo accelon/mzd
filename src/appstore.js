4a34da1ad80e7072d339b05e99c65eda891333c8
yap cheah shen <yapcheahshen@gmail.com>
2023-12-23 20:52:13 +0800

    first release

--- src/appstore.js
+++ src/appstore.js
@@ -4,7 +4,7 @@ import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 _ACC23.CacheName='v1::mzd',
 _ACC23.AppVer='23.12.23';
 _ACC23.AppName='mzd'
-_ACC23.AppTitle='我的應用'
+_ACC23.AppTitle='毛澤東選集'
 _ACC23.allptks=['mzd','mzd-en'];
 _ACC23.selectedptks=['mzd','mzd-en']
 _ACC23.activeptk='mzd'
